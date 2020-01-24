bb6611a7a63604ab27f420e660451811d57580dd
雪 风 <a1227067902@outlook.com>
2020-01-24 12:05:26 -0500

    Pushing

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+using System;
+using System.Diagnostics;
+namespace Project01_Introduction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("a=3, b=4, c=5");
+            string userInput = Console.ReadLine();
+            decimal s = (a + b + c) / 2
+            decimal Area = Math.Sqrt(s * (s - a) * (s - b) * (s - c))
+            Console.WriteLine("Area r is " + Area);
+
+
+
+
+        }
+    }
+}
